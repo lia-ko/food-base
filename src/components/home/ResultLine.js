e5d7d295239f0c6fb8c370ca8656743aa0934a1a
lia-ko <iet00100a@gmail.com>
2019-07-14 21:45:25 -0400

    classNames
    classNames

--- src/components/home/ResultLine.js
+++ src/components/home/ResultLine.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
 export function convertPrice(pricePoint){
-    
-    let dollarSigns = '';
+    let priceCategory = '';
     for(var x=0; x < pricePoint; x++){
-        dollarSigns = dollarSigns.concat('$');
+        priceCategory = priceCategory.concat('$');
     }
 
-    return dollarSigns
+    return priceCategory
 }
 
 const ResultLine = ({ restaurant }) => {
     return(
         <div className="card">
-            <img class="card-image-top" src={restaurant.image_url}  alt="restImage" />
-            <div class="card-body">
-                <h5 class="card-title">{restaurant.name}</h5>
+            <img className="card-image-top" src={restaurant.image_url}  alt="restImage" />
+            <div className="card-body">
+                <h5 className="card-title">{restaurant.name}</h5>
                 <p>{restaurant.address}</p>
                 <p>{restaurant.area}</p>
                 <p>Price: {convertPrice(restaurant.price)}</p>
